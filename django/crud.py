b54e47e623cebd1b80d7342baf24d07c653ca321
yejuPark <pyj8977@daum.net>
2023-08-28 17:32:05 +0900

    crud Read(all)

---
+++ django/crud.py
@@ -0,0 +1,35 @@
+# crud/settings.py - 앱등록
+INSTALLED_APPS = [
+    ...,
+    'posts',
+]
+
+# posts/admin.py - createsuperuser
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
+
+
+# crud/urls.py - 경로 설정
+from django.contrib import admin
+from django.urls import path
+from posts import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # Read
+    path('index/', views.index),
+]
+
+# posts/views.py - 함수
+from django.shortcuts import render, redirect
+from .models. import Post
+
+def index(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'index.html', context)
